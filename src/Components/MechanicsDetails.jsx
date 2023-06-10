e34030e53cbb486f0d10f5d95dd7a30fcb1d704e
anshulkumarx7 <anshulkumarx7@gmail.com>
2023-06-10 23:49:37 +0530

    fix changes

---
+++ src/Components/MechanicsDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../Styles/MechanicsDetails.css";
+function MechanicsDetails() {
+  return (
+    <div className='fourGearMechanics'>
+
+    </div>
+  )
+}
+
+export default MechanicsDetails
