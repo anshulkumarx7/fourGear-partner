16b0e6e2a2c9deed8291cd1109338a91c4bc194c
anshulkumarx7 <anshulkumarx7@gmail.com>
2023-06-20 23:53:16 +0530

    added joinValidation

--- src/Components/Join.jsx
+++ src/Components/Join.jsx
@@ -2,6 +2,8 @@ import React, { useRef, useState } from "react";
 import "../Styles/Join.css";
 import Image1 from "../assets/joinfourGear.svg";
 import { FiUpload } from "react-icons/fi";
+import joinValidation from "../schemas/joinValidation"
+import { CgDanger } from "react-icons/cg";
 function convertToBase64(file) {
   return new Promise((resolve, reject) => {
     const fileReader = new FileReader();
@@ -19,7 +21,7 @@ function Join() {
     name: "",
     contactNumber: "",
     email: "",
-    password:"",
+    password: "",
     address: "",
     aadharNumber: "",
     accountNumber: "",
@@ -27,24 +29,26 @@ function Join() {
     numberofMechanics: "",
   });
   const [ownerImage, setOwnerImage] = useState("");
-  const [shopImage, setshopImage] = useState("");
+  const [shImage, setShopImage] = useState("");
   const image1Ref = useRef(null);
   const image2Ref = useRef(null);
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  //handleChange
   const handleChange = (event) => {
     const { name, value } = event.target;
     setDetails((prevValue) => ({
       ...prevValue,
       [name]: value,
     }));
+    setErrors((prevErrors) => ({ ...prevErrors, [name]: "" }));
   };
   const handleImage1Click = () => {
     image1Ref.current.click();
   };
   const handleImage2Click = () => {
-    image1Ref.current.click();
-  };
-  const handleSubmit = (event) => {
-    event.preventDefault();
+    image2Ref.current.click();
   };
   const handleImage1Uplaod = async (event) => {
     const file = event.target.files[0];
@@ -56,8 +60,55 @@ function Join() {
     const base64 = await convertToBase64(file);
     setShopImage(base64);
   };
-  // console.log(shopImage);
-  // console.log(ownerImage);
+  console.log("Shop image")
+
+  console.log(shImage);
+  console.log("image")
+
+  console.log(ownerImage);
+  //handleBlur
+  const handleBlur = (event) => {
+    const { name } = event.target;
+    try {
+      if (name in details) {
+        joinValidation.validateSyncAt(name, details);
+      } else if (name in ownerImage) {
+        joinValidation.validateSyncAt(name, ownerImage);
+      } else if (name in shopImage) {
+        joinValidation.validateSyncAt(name, shopImage);
+      }
+    } catch (validationError) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        [name]: validationError.message,
+      }));
+    }
+  };
+  //handleSubmit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const formData = {
+        details,
+        ownerImage,
+        shImage
+      };
+      setLoading(true);
+      await joinValidation.validate(formData, { abortEarly: false });
+      setLoading(false);
+      // login(config);
+    } catch (validationError) {
+      setLoading(false);
+      console.log(validationError);
+      const newErrors = {};
+      validationError.inner.forEach((error) => {
+        newErrors[error.path] = error.message;
+      });
+      setErrors(newErrors);
+    }
+  };
+  console.log("errors")
+  console.log(errors);
   return (
     <div className="fourGearJoin">
       <div className="fourGearJoinLeftContainer">
@@ -77,90 +128,177 @@ function Join() {
             type="text"
             placeholder="Name"
             name="name"
+            autoComplete="off"
             onChange={handleChange}
+            onBlur={handleBlur}
             value={details.name}
           />
+          {errors.name && (
+                <div className="loginErrors">
+                  <CgDanger className="cgDanger" />
+                  {errors.name}
+                </div>
+              )}
           <input
             type="text"
             placeholder="Contact Number"
             name="contactNumber"
+            autoComplete="off"
+            onBlur={handleBlur}
             onChange={handleChange}
             value={details.contactNumber}
           />
+          {errors.contactNumber && (
+                <div className="loginErrors">
+                  <CgDanger className="cgDanger" />
+                  {errors.contactNumber}
+                </div>
+              )}
           <input
             type="email"
             placeholder="Email (optional)"
             name="email"
+            autoComplete="off"
+            onBlur={handleBlur}
             onChange={handleChange}
             value={details.email}
           />
+          {errors.email && (
+                <div className="loginErrors">
+                  <CgDanger className="cgDanger" />
+                  {errors.email}
+                </div>
+              )}
           <input
             type="password"
             placeholder="Password"
             name="password"
+            autoComplete="off"
+            onBlur={handleBlur}
             onChange={handleChange}
             value={details.password}
           />
+          {errors.password && (
+                <div className="loginErrors">
+                  <CgDanger className="cgDanger" />
+                  {errors.password}
+                </div>
+              )}
           <input
             type="text"
             placeholder="Address"
             name="address"
+            autoComplete="off"
+            onBlur={handleBlur}
             onChange={handleChange}
             value={details.address}
           />
+          {errors.address && (
+                <div className="loginErrors">
+                  <CgDanger className="cgDanger" />
+                  {errors.address}
+                </div>
+              )}
           <input
             type="text"
             placeholder="Aadhar Number"
             name="aadharNumber"
+            autoComplete="off"
+            onBlur={handleBlur}
             value={details.aadharNumber}
             onChange={handleChange}
           />
+          {errors.aadharNumber && (
+                <div className="loginErrors">
+                  <CgDanger className="cgDanger" />
+                  {errors.aadharNumber}
+                </div>
+              )}
           <input
             type="text"
             placeholder="Account Number"
             name="accountNumber"
+            autoComplete="off"
+            onBlur={handleBlur}
             value={details.accountNumber}
             onChange={handleChange}
           />
+          {errors.accountNumber && (
+                <div className="loginErrors">
+                  <CgDanger className="cgDanger" />
+                  {errors.accountNumber}
+                </div>
+              )}
           <input
             type="text"
             placeholder="IFSC Code"
             name="ifscCode"
+            autoComplete="off"
+            onBlur={handleBlur}
             value={details.ifscCode}
             onChange={handleChange}
           />
-          {/* <input type="text" placeholder="Shop Owner Photo"/> */}
+          {errors.ifscCode && (
+                <div className="loginErrors">
+                  <CgDanger className="cgDanger" />
+                  {errors.ifscCode}
+                </div>
+              )}
           <input
             type="file"
             name="shopOwnerImage"
             accept=".jpeg,.png,.jpg"
             ref={image1Ref}
+            onBlur={handleBlur}
             onChange={(e) => handleImage1Uplaod(e)}
+            // value={ownerImage}
           ></input>
           <div className="inputFile" onClick={handleImage1Click}>
-            <h4>Shop Owner photo</h4>
+            <h4>{ownerImage.length===0 ? "Shop Owner photo" : "Image Uploaded"}</h4>
             <FiUpload className="upload" />
           </div>
+          {errors.shopOwnerImage && (
+                <div className="loginErrors">
+                  <CgDanger className="cgDanger" />
+                  {errors.shopOwnerImage}
+                </div>
+              )}
           <input
             type="file"
             ref={image2Ref}
             accept=".jpeg,.png,.jpg"
             name="shopImage"
+            onBlur={handleBlur}
             onChange={(e) => handleImage2Uplaod(e)}
+            // value={shImage}
           ></input>
           <div className="inputFile" onClick={handleImage2Click}>
-            <h4>Shop photo</h4>
+            <h4>{shImage.length===0 ? "Shop Photo" : "Image Uploaded"}</h4>
             <FiUpload className="upload" />
           </div>
+          {errors.shopImage && (
+                <div className="loginErrors">
+                  <CgDanger className="cgDanger" />
+                  {errors.shopImage}
+                </div>
+              )}
           <input
             type="text"
             placeholder="Number of mechanics"
             name="numberofMechanics"
+            autoComplete="off"
+            onBlur={handleBlur}
             value={details.numberofMechanics}
             onChange={handleChange}
           />
+          {errors.numberofMechanics && (
+                <div className="loginErrors">
+                  <CgDanger className="cgDanger" />
+                  {errors.numberofMechanics}
+                </div>
+              )}
         </form>
-        <button className="fourGearJoinButton">Enter mechanics details</button>
+        <button className="fourGearJoinButton" onClick={handleSubmit}>Enter mechanics details</button>
       </div>
     </div>
   );
--- src/schemas/joinValidation.jsx
+++ src/schemas/joinValidation.jsx
@@ -1,14 +1,18 @@
 import *  as Yup from 'yup';
 
 const joinValidation =Yup.object().shape({
-    name:Yup.string().min(2,'Name should be more than 2 Characters').max(20).required('Please Enter Your Name'),
+    name:Yup.string().min(2,'Name should be more than 2 Characters').max(20).required('Please enter your Name'),
     contactNumber:Yup.string().min(10,'Invalid Contact number').max(10)
     .matches(/^\+?[1-9]\d{1,14}$/, 'Invalid contact number')
     .required('Contact number is required'),
     email:Yup.string().email('Invalid Email').required('Please enter the email'),
-    password:Yup.string().min(8,'Invalid password').max(20).required('Password Required'),
+    password:Yup.string().min(8,'Invalid password').max(20).required('Password Required')
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      " Password must be at least 8 characters long and contain [A-Z,a-z,@#]"
+    ),
     address:Yup.string().min(10,'Please enter your full address').max(30).required('Please enter your address'),
-    aadharNumber:Yup.string().min(12,'Please enter valid aadharNumber').max(12).required('Please enter your Aadhar Number'),
+    aadharNumber:Yup.string().matches(/^\d{12}$/, "Invalid Aadhaar number").required('Please enter your Aadhar Number'),
     accountNumber:Yup.string().min(10,'Invalid account number').max(25).required('Please enter your account number'),
     ifscCode:Yup.string().min(10,'Invalid IFSC Code').max(25).required('Please enter your IFSC code'),
     shopOwnerImage:Yup.mixed()
